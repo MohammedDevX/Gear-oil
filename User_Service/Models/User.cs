f6c061611591181fce992745fa540993ab0622e6
abdessamad5555 <abdessamadelouali91@gmail.com>
2026-02-17 23:13:03 +0100

    models

---
+++ User_Service/Models/User.cs
@@ -0,0 +1,20 @@
+﻿namespace User_Service.Models
+{
+    public class User
+    {
+
+        public int Id { get; set; }
+
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+
+        public string Username { get; set; }
+        public string Email { get; set; }
+
+        public string PasswordHash { get; set; }
+
+        public int Role { get; set; } // yla kan 1 admin yla kan 2 client
+
+
+    }
+}
