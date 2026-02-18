9b617b49274e5bc44dcd7720fb13ff30f49b1569
KAKACHI123EFZEF <125193387+KAKACHI123EFZEF@users.noreply.github.com>
2026-02-18 16:48:45 +0100

    Fix ocelot problem

---
+++ User_Service/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace User_Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("Index")]
+        public IActionResult Index()
+        {
+            return Ok("Hello from index");
+        }
+    }
+}
