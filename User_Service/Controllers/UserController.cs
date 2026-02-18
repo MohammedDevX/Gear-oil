1168fd792791eaf7af0bba30ec1f9bf181cca538
KAKACHI123EFZEF <125193387+KAKACHI123EFZEF@users.noreply.github.com>
2026-02-18 21:49:14 +0100

    Test Role

--- User_Service/Controllers/UserController.cs
+++ User_Service/Controllers/UserController.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using User_Service.Models;
 
 namespace User_Service.Controllers
 {
@@ -7,6 +7,17 @@ namespace User_Service.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        User user = new User()
+        {
+            UserId = 1,
+            Nom = "Bakhtaoui",
+            Prenom = "Mohammed",
+            NomUser = "Mohammed12X",
+            Email = "mohamed@gmail.com",
+            Mot_de_passe = "12345",
+            //Role = new Enums.UserRole.Admin,g
+        };
+
         [HttpGet("Index")]
         public IActionResult Index()
         {
